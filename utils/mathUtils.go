455da662912f71f45b8a0318f8919a5a2279facd
Joona Kilpinen <joona.kilpinen@fluentia.fi>
2023-12-10 02:36:42 +0200

    refactored LCM

--- utils/mathUtils.go
+++ utils/mathUtils.go
@@ -12,15 +12,10 @@ func GCD(a int, b int) int {
 }
 
 func LCM(ints []int) int {
-	l := 0
+	l := ints[0]
 	for i := 1; i < len(ints); i++ {
-		if i == 1 {
-			g := GCD(ints[0], ints[1])
-			l = ints[0] * ints[1] / g
-		} else {
-			g := GCD(l, ints[i])
-			l = l * ints[i] / g
-		}
+		g := GCD(l, ints[i])
+		l = l * ints[i] / g
 	}
 	return l
 }
